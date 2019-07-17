9a489521528c6e45b259d272bfc934e19f777732
Hellden <r.chapotin@gmail.com>
2019-07-17 11:30:12 +0200

    DEL: feature blog

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,11 +51,8 @@
 <nav>
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>Accueil</a></li>
+		<li><a class='{segment === "testing" ? "selected" : ""}' href='testing'>Testing SAPPER</a></li>
 		<li><a class='{segment === "contact" ? "selected" : ""}' href='contact'>Contact</a></li>
 		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>About</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
 	</ul>
 </nav>
